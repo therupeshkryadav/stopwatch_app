511d5d4e39b0f9950658a48ae83d1514804e3cd5
Rupesh <rupeshkryadav484@gmail.com>
2025-07-04 22:48:15 +0530

    Added fluuter_launcher_icons for setup for native launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stopwatch MVC',
+      title: 'Stopwatch App',
       debugShowCheckedModeBanner: false,
       home: StopwatchScreen(),
     );
